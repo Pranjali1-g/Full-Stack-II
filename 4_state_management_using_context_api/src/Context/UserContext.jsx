f186c3d6784a7c591c9f9c0299d359a26ef51666
Pranjali1-g <pranjaligupta0216@gmail.com>
2026-01-29 12:33:20 +0530

    commit

--- 4_state_management_using_context_api/src/Context/UserContext.jsx
+++ 4_state_management_using_context_api/src/Context/UserContext.jsx
@@ -7,7 +7,7 @@ export const UserContext = createContext(null);
 UserContext.displayName = "UserContext";
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState("Riya");
+  const [user, setUser] = useState("Pranjali");
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
